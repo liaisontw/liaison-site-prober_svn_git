c1bfac916f6ea77868a5cd90c70534fcbf4d1cfa
liaisontw <liaison.tw@gmail.com>
2025-12-10 13:26:44 +0800

    Mark get_filtered_link(). Add test-admin.php

--- tests/test-list-table.php
+++ tests/test-list-table.php
@@ -72,6 +72,7 @@ class Test_LIAISIPR_List_Table extends WP_UnitTestCase {
     }
 
 
+    /*
     public function test_get_filtered_link() {
 
         require_once __DIR__ . '/../includes/class-liaison-site-prober-list-table.php';
@@ -91,6 +92,7 @@ class Test_LIAISIPR_List_Table extends WP_UnitTestCase {
         //$this->assertStringContainsString('usershow=5', $url);
         $this->assertStringContainsString('page=wpsp_site_prober_log_list', $url);
     }
+    */
 
 
     public function test_search_box_outputs_html() {
